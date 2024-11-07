6ad02f011b3597dcf53a2e8651a31634eb38673a
anita <mateos29@example.com>
2024-11-07 21:01:24 +0100

    commit hasta try-catch

--- src/main/java/org/dis/front/MyUI.java
+++ src/main/java/org/dis/front/MyUI.java
@@ -6,44 +6,77 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
+import org.dis.back.BRException;
+import org.dis.back.EmpleadoBR;
 
 /**
- * This UI is the application entry point. A UI may either represent a browser window 
+ * This UI is the application entry point. A UI may either represent a browser window
  * (or tab) or some part of an HTML page where a Vaadin application is embedded.
  * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be
  * overridden to add component to the user interface and initialize non-component functionality.
  */
 @Theme("mytheme")
 public class MyUI extends UI {
 
-    private TextField creaLabel(String texto)
-    {
-        TextField etiqueta= new TextField();
+    private TextField creaLabel (String texto){
+        TextField etiqueta = new TextField();
         etiqueta.setCaption(texto);
         return etiqueta;
     }
-    @Override
 
+    @Override
     protected void init(VaadinRequest vaadinRequest) {
         final VerticalLayout layout = new VerticalLayout();
-        
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
+        final HorizontalLayout salarioBruto = new HorizontalLayout();
+        final HorizontalLayout salarioNeto = new HorizontalLayout();
+        final VerticalLayout salarioBrutoContenedor = new VerticalLayout();
+        final VerticalLayout salarioNetoContenedor = new VerticalLayout();
 
-        Button button = new Button("Click Me");
-        button.addClickListener(e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
+        TextField tipo = creaLabel("Tipo de empleado");
+        TextField ventasMes = creaLabel("Ventas del mes");
+        TextField horasExtras = creaLabel("Horas extras");
+
+        salarioBruto.addComponents(tipo, ventasMes, horasExtras);
+
+        Button button1 = new Button("calcular");
+        button1.addClickListener(e -> {
+
         });
-        
-        layout.addComponents(name, button);
-        
+
+        Button botonSalarioBruto  = new Button("Calcula Salario Bruto");
+        botonSalarioBruto.addClickListener(e -> {
+            String tipoEmpleadoIn = tipo.getValue();
+            double ventasMesIn = Double.parseDouble(ventasMes.getValue());
+            double horasExtraIn = Double.parseDouble(horasExtras.getValue());
+
+            EmpleadoBR empleado = new EmpleadoBR();
+
+            try {
+                double resultado = empleado.calculaSalarioBruto(tipoEmpleadoIn, ventasMesIn, horasExtraIn);
+                Label labelSalarioBruto = new Label("El salario bruto obtenido es: " + resultado + "€");
+                salarioBrutoContenedor.addComponent(labelSalarioBruto);
+            } catch (BRException ex) {
+                Label labelSalarioBruto = new Label(ex.getMessage());
+                salarioBrutoContenedor.addComponent(labelSalarioBruto);
+            }
+        });
+
+        Button botonSalarioNeto  = new Button("Calcula Salario Neto");
+        botonSalarioNeto.addClickListener(e -> {});
+
+        salarioBrutoContenedor.addComponents(salarioBruto, botonSalarioBruto);
+        salarioNetoContenedor.addComponents(salarioNeto, botonSalarioNeto);
+
+        TabSheet tabs = new TabSheet();
+        tabs.addTab(salarioBrutoContenedor).setCaption("Calcula Salario Bruto");
+        tabs.addTab(salarioNetoContenedor).setCaption("Calcula Salario Neto");
+
+        layout.addComponents(tabs);
+
+
+
         setContent(layout);
     }
 
